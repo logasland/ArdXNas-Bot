74207226c0151828dae603018b86cd05f47a40c9
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-05 17:36:36 +0700

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -1,11 +1,10 @@
-let brainly = require("brainly-scraper")
+/*let brainly = require("brainly-scraper")
 let handler = async function (m, { text, usedPrefix, command }) {
 if (!text) return m.reply(`uhm.. soalnya mana?\n\ncontoh:\n${usedPrefix + command} apa itu javascript?`)
 let res = await brainly(text)
 let answer = res.data.map((v, i) => `_*PERTANYAAN KE ${i + 1}*_\n${v.pertanyaan}\n${v.jawaban.map((v,i) => `*JAWABAN KE ${i + 1}*\n${v.text}`).join('\n')}`).join('\n\n•------------•\n\n')
 m.reply(answer)
 }
-
 handler.help = ['brainly <soal>']
 handler.tags = ['internet']
 handler.command = /^brainly$/i
@@ -13,7 +12,7 @@ handler.limit = true
 
 module.exports = handler
 
-
+*/
 /*const Brainly = require('brainly-scraper-v2')
 const brain = new Brainly("id")
 let handler = async function (m, { text, usedPrefix, command }) {
