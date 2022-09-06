b2222016ab1a9db5c1793a3a93db87f8b239a0d3
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-06 10:45:42 +0700

    Pesan bang😂

---
+++ plugins/_pesanbang.js
@@ -0,0 +1,8 @@
+let fs = require('fs')
+let handler = async(m, { conn }) => {
+conn.sendFile(m.chat, fs.readFileSync('./media/src_Ara.mp3'), '', '', m, true)
+}
+handler.customPrefix = /^(Pesan bang|Pesan)?$/i
+handler.command = new RegExp
+
+module.exports = handler
