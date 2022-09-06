016fc8b2a56b516b5c020ba5868b737ba19f3230
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-06 07:11:20 +0700

    ArdXNas-Bot🎉

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,10 +1,12 @@
+let fs = require('fs')
 let handler = async (m, { conn }) => {
-let ArdXNasBot = `*── 「 INFO 」 ──*
+let ArdXNasBot = `*─ 「 INFO - BOT - OWNER 」 ─*
 ▢ *IG NASIR*:
 ▢ *IG ARDI*:
 • *SC*: _https://github.com/logasland/ArdXNas-Bot_
 `
-conn.sendButton(m.chat, ArdXNasBot, wm, null, [['Menu', '.menu'],['Owner', '.owner']], m)
+conn.sendFile(m.chat, fs.readFileSync('./src/blackpink.png'), 'file.AXS', ArdXNasBot, m)
+//conn.sendButton(m.chat, ArdXNasBot, wm, null, [['Menu', '.menu'],['Owner', '.owner']], m)
 }
 handler.help = ['infosc']
 handler.tags = ['main']
