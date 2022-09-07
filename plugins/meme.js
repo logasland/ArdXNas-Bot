4197db0aa0adcf32d129d53de9811702d594da0b
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-07 13:03:19 +0700

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,13 +1,10 @@
 let fetch = require("node-fetch")
 let handler = async (m, { conn }) => {
-  let res = await fetch(global.API('https://some-random-api.ml', '/meme'))
-  await m.reply(global.wait)
-  if (!res.ok) throw await res.text()
+  let res = await fetch('https://candaan-api.vercel.app/api/image/random')
   let json = await res.json()
-  if (!json.image) throw 'Err!'
-  conn.sendFile(m.chat, json.image, 'meme.png', json.caption, m)
+  if (!json.data.url) throw 'Err!'
+  conn.sendFile(m.chat, json.data.url, 'meme.png', json.method, m)
 }
-
 handler.help = ['meme']
 handler.tags = ['random']
 handler.register = false
