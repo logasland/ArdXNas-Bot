2b1c0047cea83ae810af72089306abc56c20ea79
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-09 17:27:13 +0700

    Update diamond.js

--- plugins/diamond.js
+++ plugins/diamond.js
@@ -1,7 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
+let name = await conn.getName(m.sender)
 let thumb = 'https://telegra.ph/file/a75f1fae1fe7232b317d3.jpg'
-let ArdXNasBot = `Hai ${ucapan()} *NaRaa SHOP* 💕 dsni :D
+let ArdXNasBot = `Hai kak ${name}, Aku *NaRaa SHOP* 💕 dsni :D
 
 *Top UP All Game Aman terpercaya*
 *Tersedia diamond game Lengkap*
@@ -19,30 +20,18 @@ Wa.me/6288220924312
 *_Join Group kami untuk melakukan pembelian_*
 https://chat.whatsapp.com/HPFwkIhRkzGFHmZ9rDVQ3U
 `
-conn.sendFile(m.chat, await(await fetch(thumb)).buffer(), 'file.AXS', ArdXNasBot, m)
+conn.sendMessage(m.chat, {
+        image: await(await fetch(thumb)).buffer(),
+        caption: ArdXNasBot,
+        buttons: [
+          {buttonId: `.dmml`, buttonText: {displayText: '💎DIAMOND ML'}, type: "RESPONSE"},
+          {buttonId: `.dmff`, buttonText: {displayText: 'DIAMOND FF💎'}, type: "RESPONSE"}
+        ],
+        headerType: 'IMAGE'
+  }, { quoted: m })
 }
 handler.help = ['diamond']
 handler.tags = ['main']
-handler.command = /^diamond|pulsa(game|ml|freefire)$/i
+handler.command = /^diamond|dm|pulsa(game|games)$/i
 
 module.exports = handler
-
-
-function ucapan() {
-        const hour_now = moment.tz('Asia/Jakarta').format('HH')
-        var ucapanWaktu = 'Pagi kak'
-        if (hour_now >= '03' && hour_now <= '10') {
-          ucapanWaktu = 'Pagi kak'
-        } else if (hour_now >= '10' && hour_now <= '15') {
-          ucapanWaktu = 'Siang kak'
-        } else if (hour_now >= '15' && hour_now <= '17') {
-          ucapanWaktu = 'Sore kak'
-        } else if (hour_now >= '17' && hour_now <= '18') {
-          ucapanWaktu = 'Selamat Petang kak'
-        } else if (hour_now >= '18' && hour_now <= '23') {
-          ucapanWaktu = 'Malam kak'
-        } else {
-          ucapanWaktu = 'Selamat Malam!'
-        }	
-        return ucapanWaktu
-}
