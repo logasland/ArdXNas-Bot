6f37f94b3402d03d2a2752771aa518ecdbd47e9d
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-12 20:38:59 +0700

    Update _pesanbang.js

--- plugins/_pesanbang.js
+++ plugins/_pesanbang.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 let handler = async(m, { conn }) => {
-conn.sendFile(m.chat, fs.readFileSync('./media/src_Ara.mp3'), '', '', m, true)
+conn.sendFile(m.chat, fs.readFileSync('./media/AUD-20220912-WA0046.mp3'), '', '', m, true)
 }
 handler.customPrefix = /(Pesan)/i
 handler.command = new RegExp
