42521ac322277fc2e1a00d0c9a8b38099274af50
logasland <62803820+logasland@users.noreply.github.com>
2022-10-15 12:23:19 +0700

    Update youtube.js

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -3,14 +3,24 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 	if (!text) throw `*Link Youtube nya mana?*\n\n*contoh:https://youtu.be/oa04wbLyENo`
 	let res = await fetch(`https://itztobz.me/api/youtube-mp4?url=` + text )
 	let json = await res.json()
+let Naon = `*Title :* ${json.title}\n\n_${wm}_
+`
+conn.sendMessage(m.chat, {
+        image: await(await fetch(json.thumbnail)).buffer(),
+        caption: Naon,
+        buttons: [
+          {buttonId: `.get ${json.result[0].download}`, buttonText: {displayText: `${json.result[0].quality} (${json.result[0].filesizeMB})`}, type: "RESPONSE"},
+        ],
 
-conn.sendFile(m.chat, json.result[0].download, 'fb.mp4', `*Title : ${json.title}*\n\n_${wm}_`, m)
+        headerType: 'IMAGE'
+
+  }, { quoted: m })
 }
-handler.help = ['yt'].map(v => v + ' <url>')
-
+handler.help = ['youtube'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-
 handler.limit = true
 handler.group = true
 
-handler.command = /^(yt|ytmp4|ytv)$/i
+handler.command = /^(youtube|ytmp4|yt)$/i
+
+module.exports = handler
